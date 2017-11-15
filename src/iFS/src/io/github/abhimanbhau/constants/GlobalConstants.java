dd6557ed2be3ef920ae4f0e32611d0ff1f1cb4a6
Abhiman Kolte <abhimanbhau@icloud.com>
2017-11-14 19:57:09 -0800

    Implemented the Barebone logging subsystem

---
+++ src/iFS/src/io/github/abhimanbhau/constants/GlobalConstants.java
@@ -0,0 +1,25 @@
+package io.github.abhimanbhau.constants;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class GlobalConstants {
+    public static String DebugString = "{iFS} Debug: ";
+    public static String ErrorString = "{iFS} Error: ";
+
+    public static String getDateOrTime(boolean date)
+    {
+        if(date) {
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yy-mm-dd");
+            return timeFormatter.format(LocalDate.now());
+        }
+        else
+        {
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH-mm-ss");
+            return timeFormatter.format(LocalDateTime.now());
+        }
+    }
+}
--- src/iFS/src/io/github/abhimanbhau/logging/Logger.java
+++ src/iFS/src/io/github/abhimanbhau/logging/Logger.java
@@ -1,4 +1,29 @@
 package io.github.abhimanbhau.logging;
 
+import io.github.abhimanbhau.constants.GlobalConstants;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logger {
+    private FileWriter _writer;
+    private BufferedWriter _buffer;
+
+    public Logger(String path) throws IOException {
+        _buffer = new BufferedWriter(new FileWriter(path));
+    }
+
+    public void Finish() throws IOException {
+        _buffer.flush();
+        _buffer.close();
+    }
+
+    public void LogError(String error) throws IOException {
+        _buffer.write(GlobalConstants.ErrorString + error);
+    }
+
+    public void LogDebug(String debug) throws IOException {
+        _buffer.write(GlobalConstants.DebugString + debug);
+    }
 }
